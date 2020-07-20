70bf70c9e57250fa5d8762db9859829d915d41a6
unknown <safrany89@hotmail.com>
2020-07-20 19:29:22 +0200

    d

--- js/pictures.js
+++ js/pictures.js
@@ -7,8 +7,11 @@ inputCityName.setAttribute("placeholder", "városnév");
 
 let contentCityName = inputCityName.value;
 
-if (contentCityName== ("Bécs" || "Bécs "))
+if (contentCityName== "Bécs")
 
+    alert("Igen Bécs!!! Ügyes vagy Cuki! :) ");
+
+    else if (contentCityName== "Bécs ")
     alert("Igen Bécs!!! Ügyes vagy Cuki! :) ");
 
     else
