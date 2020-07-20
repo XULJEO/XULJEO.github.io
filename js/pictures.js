04560602613c1c8baca48027425111c97aacd446
unknown <safrany89@hotmail.com>
2020-07-20 19:13:16 +0200

    s

---
+++ js/pictures.js
@@ -0,0 +1,18 @@
+function game ()
+{
+
+let inputCityName = document.querySelector("#church");
+
+inputCityName.setAttribute("placeholder", "városnév");
+
+let contentCityName = inputCityName.value;
+
+if (contentCityName=="Bécs")
+
+    alert("Igen Bécs!!! Ügyes vagy Cuki! :) ");
+
+    else
+
+    alert("Sajnos nem jó válasz. Próbáld meg újra! :)");
+
+}
